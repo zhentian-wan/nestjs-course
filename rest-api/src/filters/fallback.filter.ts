bd33cd08cd375f6ab4ec62a1803be43c9f1d5089
zhentian-wan <answer881215@gmail.com>
2020-01-15 08:30:14 +0200

    fallback exception fitler

---
+++ rest-api/src/filters/fallback.filter.ts
@@ -0,0 +1,19 @@
+import { Catch, ExceptionFilter, ArgumentsHost } from "@nestjs/common";
+
+@Catch()
+export class FallbackExpectionFilter implements ExceptionFilter {
+    catch(exception: any, host: ArgumentsHost) {
+        console.log('fallback exception handler triggered',
+        JSON.stringify(exception));
+
+        const ctx = host.switchToHttp();
+        const response = ctx.getResponse();
+
+        return response.status(500)
+            .json({
+                statusCode: 500,
+                createdBy: "FallbackExceptionFilter",
+                errorMessage: exception.message ? exception.message: 'Unexpected error ocurred';
+            });
+    }
+}
--- rest-api/src/main.ts
+++ rest-api/src/main.ts
@@ -2,12 +2,16 @@ import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './filters/http-exception.filter';
+import { FallbackExpectionFilter } from './filters/fallback.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api');
-  app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalFilters(
+    new FallbackExpectionFilter(),
+    new HttpExceptionFilter(),
+  );
 
   await app.listen(9000);
 }
