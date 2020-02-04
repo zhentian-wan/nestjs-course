a282dd50c0f33da15cd95c3729b7a0e961296623
zhentian-wan <answer881215@gmail.com>
2020-02-04 22:35:10 +0200

    add dark theme

--- rest-api/src/filters/fallback.filter.ts
+++ rest-api/src/filters/fallback.filter.ts
@@ -1,19 +1,22 @@
-import { Catch, ExceptionFilter, ArgumentsHost } from "@nestjs/common";
+import { Catch, ExceptionFilter, ArgumentsHost } from '@nestjs/common';
 
 @Catch()
 export class FallbackExpectionFilter implements ExceptionFilter {
-    catch(exception: any, host: ArgumentsHost) {
-        console.log('fallback exception handler triggered',
-        JSON.stringify(exception));
+  catch(exception: any, host: ArgumentsHost) {
+    console.log(
+      'fallback exception handler triggered',
+      JSON.stringify(exception),
+    );
 
-        const ctx = host.switchToHttp();
-        const response = ctx.getResponse();
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse();
 
-        return response.status(500)
-            .json({
-                statusCode: 500,
-                createdBy: "FallbackExceptionFilter",
-                errorMessage: exception.message ? exception.message: 'Unexpected error ocurred';
-            });
-    }
+    return response.status(500).json({
+      statusCode: 500,
+      createdBy: 'FallbackExceptionFilter',
+      errorMessage: exception.message
+        ? exception.message
+        : 'Unexpected error ocurred',
+    });
+  }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,16 @@
-import {Component, OnInit} from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent implements OnInit {
+  dark: boolean = false;
 
+  constructor() {}
 
+  ngOnInit() {}
 
-    constructor() {
-
-    }
-
-    ngOnInit() {
-
-
-    }
-
-    logout() {
-
-    }
-
+  logout() {}
 }
